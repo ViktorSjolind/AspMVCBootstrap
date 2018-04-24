a01186a5365432ce783b81bf2322d0c0ad73b5b8
ViktorSjolind <viktor.sjolind@gmail.com>
2018-04-24 18:02:20 +0300

    Add project files.

---
+++ AspMVCBootstrap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspMVCBootstrap.Startup))]
+namespace AspMVCBootstrap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
